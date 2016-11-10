7a4532b58c942ccb76c0349dfa497756c58f34a2
Roman Galeev <jamhed@2600hz.com>
2016-11-10 21:49:26 +0100

    more split

---
+++ Device/BasicCall.php
@@ -0,0 +1,24 @@
+<?php
+namespace KazooTests\Applications\Callflow;
+use \MakeBusy\Common\Log;
+
+class BasicCallTest extends CallflowTestCase {
+
+    public function testMain() {
+        $channels    = self::getChannels();
+        $no_device_id = self::$no_device->getId();
+
+        $uuid_base = "testCallBasic-";
+
+        foreach (self::getSipTargets() as $sip_uri) {
+            $target = self::MILLIWATT_NUMBER . '@' . $sip_uri;
+            Log::debug("trying target %s", $target);
+            $options = array("origination_uuid" => $uuid_base . Utils::randomString(8));
+            $uuid = $channels->gatewayOriginate($no_device_id, $target, $options);
+            $channel = $channels->waitForOriginate($uuid);
+            $this->assertInstanceOf("\\MakeBusy\\FreeSWITCH\\Channels\\Channel", $channel);
+            $channel->hangup();
+        }
+    }
+
+}
--- Device/Registrations.php
+++ Device/Registrations.php
@@ -1,13 +1,11 @@
 <?php
 namespace KazooTests\Applications\Callflow;
 use \MakeBusy\Common\Log;
-use \MakeBusy\FreeSWITCH\Esl\Connection as EslConnection;
 
 class DeviceTest extends CallflowTestCase {
 
-    public function testRegistrations() {
-        Log::notice("%s", __METHOD__);
-        $gateways = Profiles::getProfile('auth')->getGateways();
+    public function testMain() {
+        $gateways = $this->getGateways("auth");
         $register_device_id = self::$register_device->getId();
         $this->assertTrue($gateways->findByName($register_device_id)->register());
     }
