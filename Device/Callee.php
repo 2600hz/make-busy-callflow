4fecaece5c82f04bbf08c13e7808feb47315cfe6
Roman Galeev <jamhed@2600hz.com>
2016-11-11 18:43:00 +0100

    new api

--- Device/Callee.php
+++ Device/Callee.php
@@ -1,55 +1,32 @@
 <?php
 namespace KazooTests\Applications\Callflow;
-use \MakeBusy\Common\Utils;
 use \MakeBusy\Common\Log;
 
 class CalleeTest extends CallflowTestCase {
 
     public function testDisabled() {
-        $channels = self::getChannels("auth");
-        $a_device_id = self::$a_device->getId();
-        $b_username  = self::$b_device->getSipUsername();
-
         self::$b_device->disableDevice();
-
-        $gateways  = Profiles::getProfile('auth')->getGateways();
-        $this->assertFalse($gateways->findByName($b_device_id)->register());
-
-        $uuid_base = "testCalleeDisabled-";
+        $this->assertFalse($b_device->getGateway()->register());
 
         foreach (self::getSipTargets() as $sip_uri) {
             $target  = self::B_EXT .'@'. $sip_uri;
-            Log::debug("trying target %s", $target);
-            $options = array("origination_uuid" => $uuid_base . Utils::randomString(8));
-            $uuid    = $channels->gatewayOriginate($a_device_id, $target, $options);
-            $channel = $channels->waitForInbound($b_username);
-            $this->assertEmpty($channel);
+            $ch_o = $a_device->originate($target, $this->originate_uuid());
+            $ch_i = $b_device->waitForInbound();
+            $this->assertEmpty($ch_i);
         }
     }
 
     public function testEnabled() {
-        $channels = self::getChannels("auth");
-        $a_device_id = self::$a_device->getId();
-        $b_username  = self::$b_device->getSipUsername();
-        $b_device_id = self::$b_device->getId();
-
         self::$b_device->enableDevice();
-
-        $gateways  = Profiles::getProfile('auth')->getGateways();
-        $this->assertTrue($gateways->findByName($b_device_id)->register());
-
-        $uuid_base = "testCalleeEnabled-";
+        $this->assertTrue($b_device->getGateway()->register());
 
         foreach (self::getSipTargets() as $sip_uri) {
             $target  = self::B_EXT .'@'. $sip_uri;
-            Log::debug("trying target %s", $target);
-            $options = array("origination_uuid" => $uuid_base . Utils::randomString(8));
-            $uuid    = $channels->gatewayOriginate($a_device_id, $target, $options);
-            $b_channel = $channels->waitForInbound($b_username);
-            $this->assertInstanceOf("\\MakeBusy\\FreeSWITCH\\Channels\\Channel", $b_channel);
-            $a_channel = $this->ensureAnswer("auth", $uuid, $b_channel);
-            $this->ensureTwoWayAudio($a_channel, $b_channel);
-            $this->hangupBridged($a_channel, $b_channel);
+            $ch_a = $a_device->originate($target, $this->originate_uuid());
+            $ch_b = $b_device->ensureInbound();
+            $this->ensureAnswer($ch_a, $ch_b);
+            $this->ensureTwoWayAudio($ch_i, $ch_b);
+            $this->hangupBridged($ch_i, $ch_b);
         }
     }
 
