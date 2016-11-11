3f237d1a5c04c074eb13ff7c19c929fb00d204ed
Roman Galeev <jamhed@2600hz.com>
2016-11-11 18:08:41 +0100

    bugfix

--- Device/BasicCall.php
+++ Device/BasicCall.php
@@ -7,7 +7,7 @@ class BasicCallTest extends CallflowTestCase {
     public function testMain() {
         foreach (self::getSipTargets() as $sip_uri) {
             $target = self::MILLIWATT_NUMBER . '@' . $sip_uri;
-            $channel = self::$no_device->originate($no_device_id, $target, $this->originate_uuid());
+            $channel = self::$no_device->originate($target, $this->originate_uuid());
             $this->hangupChannels($channel);
         }
    }
