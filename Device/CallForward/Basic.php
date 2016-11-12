2c7e2942ce53d086441e3194cac0c6d43a80faef
Roman Galeev <jamhed@2600hz.com>
2016-11-12 03:48:10 +0100

    more tests

--- Device/CallForward/Basic.php
+++ Device/CallForward/Basic.php
@@ -6,26 +6,19 @@ use \MakeBusy\Common\Log;
 class CallForwardTest extends CallflowTestCase {
 
     public function testMain() {
-        $channels = self::getChannels("auth");
-
-        $a_device_id = self::$a_device->getId();
-        $b_device_id = self::$b_device->getId();
-        $c_username = self::$c_device->getSipUsername();
-
         self::$b_device->resetCfParams(self::C_EXT);
-        $uuid_base = "testCfBasic-";
 
         foreach (self::getSipTargets() as $sip_uri) {
             $target = self::B_EXT .'@' . $sip_uri;
-            Log::debug("trying target %s", $target);
-            $options = array("origination_uuid" => $uuid_base . Utils::randomString(8));
-            $uuid = $channels->gatewayOriginate($a_device_id, $target, $options);
-            $c_channel = $channels->waitForInbound($c_username);
-            $this->assertInstanceOf("\\MakeBusy\\FreeSWITCH\\Channels\\Channel", $c_channel);
-            $a_channel = $this->ensureAnswer("auth", $uuid, $c_channel);
-            $this->ensureTwoWayAudio($a_channel, $c_channel);
-            $this->hangupBridged($a_channel, $c_channel);
+
+            $ch_a = self::ensureChannel( self::$a_device->originate($target) );
+            $ch_c = self::ensureChannel( self::$c_device->waitForInbound() );
+
+            self::ensureAnswer($ch_a, $ch_c);
+            self::ensureTwoWayAudio($ch_a, $ch_c);
+            self::hangupBridged($ch_a, $ch_c);
         }
+
         self::$b_device->resetCfParams();
     }
 
