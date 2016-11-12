a639c603b35b9456e1e837df66dd331058dc5c34
Roman Galeev <jamhed@2600hz.com>
2016-11-12 04:22:18 +0100

    more tests

--- Device/CallForward/Substitute.php
+++ Device/CallForward/Substitute.php
@@ -5,42 +5,25 @@ use \MakeBusy\Common\Log;
 
 class CallForwardSubstituteTest extends CallflowTestCase {
 
-    public function testTrue() {
-        $channels = self::getChannels("auth");
-        $a_device_id = self::$a_device->getId();
-        $b_device_id = self::$b_device->getId();
-        $c_device_id = self::$c_device->getId();
-        $b_username  = self::$b_device->getSipUsername();
-        $c_username  = self::$c_device->getSipUsername();
-
+    public function testFalse() {
         self::$b_device->resetCfParams(self::C_EXT);
         self::$b_device->setCfParam("substitute", FALSE);
 
-        $uuid_base = "testCfSubstituteFalse-";
-
         foreach (self::getSipTargets() as $sip_uri) {
             $target  = self::B_EXT .'@'. $sip_uri;
-            Log::debug("trying target %s", $target);
-            $options = array("origination_uuid" => $uuid_base . Utils::randomString(8));
-            $uuid = $channels->gatewayOriginate($a_device_id, $target, $options);
-            $b_channel = $channels->waitForInbound($b_username);
-            $c_channel = $channels->waitForInbound($c_username);
-            $this->assertInstanceOf("\\MakeBusy\\FreeSWITCH\\Channels\\Channel", $b_channel);
-            $this->assertInstanceOf("\\MakeBusy\\FreeSWITCH\\Channels\\Channel", $c_channel);
-            $a_channel = $this->ensureAnswer("auth", $uuid, $c_channel);
-            $this->ensureTwoWayAudio($a_channel, $c_channel);
-            $this->hangupBridged($a_channel, $c_channel);
+
+            $ch_a = self::ensureChannel( self::$a_device->originate($target) );
+            $ch_b = self::ensureChannel( self::$b_device->waitForInbound() );
+            $ch_c = self::ensureChannel( self::$c_device->waitForInbound() );
+            
+            self::ensureAnswer($ch_a, $ch_c);
+            self::ensureTwoWayAudio($ch_a, $ch_c);
+            self::hangupBridged($ch_a, $ch_c);
         }
+        self::$b_device->resetCfParams();
     }
 
-    public function testCfSubstituteTrue(){
-        $channels  = self::getChannels("auth");
-        $a_device_id = self::$a_device->getId();
-        $b_device_id = self::$b_device->getId();
-        $c_device_id = self::$c_device->getId();
-        $b_username  = self::$b_device->getSipUsername();
-        $c_username  = self::$c_device->getSipUsername();
-
+    public function testTrue(){
         self::$b_device->resetCfParams(self::C_EXT);
         self::$b_device->setCfParam("substitute", TRUE);
 
@@ -48,17 +31,16 @@ class CallForwardSubstituteTest extends CallflowTestCase {
 
         foreach (self::getSipTargets() as $sip_uri) {
             $target  = self::B_EXT .'@'. $sip_uri;
-            Log::debug("trying target %s", $target);
-            $options = array("origination_uuid" => $uuid_base . Utils::randomString(8));
-            $uuid = $channels->gatewayOriginate($a_device_id, $target, $options);
-            $c_channel = $channels->waitForInbound($c_username);
-            $b_channel = $channels->waitForInbound($b_username);
-            $this->assertEmpty($b_channel);
-            $this->assertInstanceOf("\\MakeBusy\\FreeSWITCH\\Channels\\Channel", $c_channel);
-            $a_channel = $this->ensureAnswer("auth", $uuid, $c_channel);
-            $this->ensureTwoWayAudio($a_channel, $c_channel);
-            $this->hangupBridged($a_channel, $c_channel);
+
+            $ch_a = self::ensureChannel( self::$a_device->originate($target) );
+            $ch_b = self::assertNull( self::$b_device->waitForInbound() );
+            $ch_c = self::ensureChannel( self::$c_device->waitForInbound() );
+
+            self::ensureAnswer($ch_a, $ch_c);
+            self::ensureTwoWayAudio($ch_a, $ch_c);
+            self::hangupBridged($ch_a, $ch_c);
         }
+        self::$b_device->resetCfParams();
     }
 
 }
