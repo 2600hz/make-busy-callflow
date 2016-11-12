17388a46bd1fb0f008876c740dd5cdc297d4ef83
Roman Galeev <jamhed@2600hz.com>
2016-11-12 01:12:22 +0100

    more tests

--- Device/Callee.php
+++ Device/Callee.php
@@ -23,7 +23,7 @@ class CalleeTest extends CallflowTestCase {
         foreach (self::getSipTargets() as $sip_uri) {
             $target  = self::B_EXT .'@'. $sip_uri;
             $ch_a = self::ensureChannel( self::$a_device->originate($target) );
-            $ch_b = self::ensureChannel( self::$b_device->waitForInbound(self::B_EXT) );
+            $ch_b = self::ensureChannel( self::$b_device->waitForInbound() );
             self::ensureAnswer($ch_a, $ch_b);
             self::ensureTwoWayAudio($ch_a, $ch_b);
             self::hangupBridged($ch_a, $ch_b);
