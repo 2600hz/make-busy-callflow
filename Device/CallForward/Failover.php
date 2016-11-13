984a800810decf588a9594ac9ad1c3cfde8b4a06
Roman Galeev <jamhed@2600hz.com>
2016-11-13 19:22:13 +0100

    renaming

--- Device/CallForward/Failover.php
+++ Device/CallForward/Failover.php
@@ -4,20 +4,24 @@ use \MakeBusy\Common\Log;
 
 class FailoverTest extends DeviceTestCase {
 
-    public function testMain() {
+    public function setUp() {
         self::$no_device->resetCfParams(self::C_EXT);
         self::$no_device->setCfParam("failover", TRUE);
         self::$no_device->setCfParam("enabled", FALSE);
+    }
 
-        foreach (self::getSipTargets() as $sip_uri) {
-            $target = self::NO_EXT .'@'. $sip_uri;
-            $ch_a = self::ensureChannel( self::$a_device->originate($target) );
-            $ch_c = self::ensureChannel( self::$c_device->waitForInbound() );
+    public function tearDown() {
+        self::$no_device->resetCfParams();
+    }
 
-            self::ensureAnswer($ch_a, $ch_c);
-            self::ensureTwoWayAudio($ch_a, $ch_c);
-            self::hangupBridged($ch_a, $ch_c);
-        }
+    public function main($sip_uri) {
+        $target = self::NO_EXT .'@'. $sip_uri;
+        $ch_a = self::ensureChannel( self::$a_device->originate($target) );
+        $ch_c = self::ensureChannel( self::$c_device->waitForInbound() );
+
+        self::ensureAnswer($ch_a, $ch_c);
+        self::ensureTwoWayAudio($ch_a, $ch_c);
+        self::hangupBridged($ch_a, $ch_c);
     }
 
 }
