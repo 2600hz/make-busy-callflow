36e47af17c4b93cebc5fa8ba3bc001909008f633
Roman Galeev <jamhed@2600hz.com>
2017-01-10 23:19:27 +0100

    reset vmbox after tests
    - formatting

--- Voicemail/Setup.php
+++ Voicemail/Setup.php
@@ -3,6 +3,11 @@ namespace KazooTests\Applications\Callflow;
 use \MakeBusy\Common\Log;
 
 class Setup extends VoicemailTestCase {
+
+    public function tearDown() {
+        self::$b_voicemail_box->resetVoicemailBox();
+    }
+
     public function main($sip_uri) {
     }
 }
