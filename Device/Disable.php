9a8f6ae0f8ae956de5c08eb95c0ff6f36106bf21
lazedo <luis.azedo@factorlusitano.com>
2018-01-31 00:31:28 +0000

    update disable test

--- Device/Disable.php
+++ Device/Disable.php
@@ -6,20 +6,20 @@ use \MakeBusy\Common\Log;
 
 class Disable extends DeviceTestCase {
 
-    public function setUpTest() {
-        self::$b_device->disableDevice();
-        self::assertFalse(self::$b_device->getGateway()->register());
-    }
-
     public function tearDownTest() {
-        self::$b_device->enableDevice();
+    	self::$b_device->enableDevice();
+    	self::$b_device->getGateway()->register();
     }
 
     public function main($sip_uri) {
-        $target = self::B_EXT .'@'. $sip_uri;
+    	self::$b_device->disableDevice();
+    	self::assertFalse(self::$b_device->getGateway()->register());
+    	$target = self::B_EXT .'@'. $sip_uri;
         $channel_a = self::ensureChannel( self::$a_device->originate($target) );
         $channel_b = self::$b_device->waitForInbound();
         self::assertEmpty($channel_b);
+        self::$b_device->enableDevice();
+        self::assertTrue(self::$b_device->getGateway()->register());
     }
 
 }
