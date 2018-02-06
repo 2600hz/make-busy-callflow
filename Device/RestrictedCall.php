7bc59cc7ffe65d228ed9aad2625b6629566c8bc7
lazedo <luis.azedo@factorlusitano.com>
2018-02-06 16:21:02 +0000

    endpoint is invalidated and unregistered after change to restricted call

--- Device/RestrictedCall.php
+++ Device/RestrictedCall.php
@@ -7,14 +7,17 @@ use \MakeBusy\Common\Log;
 class RestrictedCall extends DeviceTestCase {
 
     public function setUpTest() {
-        self::$a_device->setRestriction("caribbean");
+        self::$a_device->setRestriction("caribbean");        
+        self::$a_device->getGateway()->register();
     }
 
     public function tearDownTest() {
         self::$a_device->resetRestrictions();
+        self::$a_device->getGateway()->register();
     }
 
     public function main($sip_uri) {
+//    	$this->assertTrue(self::$a_device->getGateway()->register());
         $target  = self::RESTRICTED_NUMBER .'@'. $sip_uri;
         $channel_a = self::ensureChannel( self::$a_device->originate($target) );
         $channel_b = self::$offnet_resource->waitForInbound('+1' . self::RESTRICTED_NUMBER);
