821c5de0fd1387712149269b1e229353b220e596
lazedo <luis.azedo@factorlusitano.com>
2019-02-15 11:32:45 +0000

    more descriptive in emergency test

--- Device/CallerIdEmergency.php
+++ Device/CallerIdEmergency.php
@@ -10,10 +10,12 @@ class CallerIdEmergency extends DeviceTestCase {
         $target  = self::EMERGENCY_NUMBER .'@'. $sip_uri;
         $channel_a = self::ensureChannel( self::$a_device->originate($target) );
         $channel_b = self::ensureChannel( self::$emergency_resource->waitForInbound(self::EMERGENCY_NUMBER) );
-        self::assertEquals(
-            self::$a_device->getCidParam("emergency")->number,
-            urldecode($channel_b->getEvent()->getHeader("Caller-Caller-ID-Number"))
-        );
+        $a_cidnum = self::$a_device->getCidParam("emergency")->number;
+        $a_cidname = self::$a_device->getCidParam("emergency")->name;
+        $b_cidnum = urldecode($channel_b->getEvent()->getHeader("Caller-Caller-ID-Number"));
+        $b_cidname = urldecode($channel_b->getEvent()->getHeader("Caller-Caller-ID-Name"));
+        self::assertEquals($a_cidnum, $b_cidnum, "Emergency Number - " . $a_cidnum . " - " . $b_cidnum);
+        self::assertEquals($a_cidname, $b_cidname, "Emergency Name - " . $a_cidname . " - " . $b_cidname);
         self::ensureAnswer($channel_a, $channel_b);
         self::hangupBridged($channel_a, $channel_b);
     }
--- User/CallerIdEmergencyUser.php
+++ User/CallerIdEmergencyUser.php
@@ -23,10 +23,12 @@ class CallerIdEmergencyUser extends UserTestCase {
         $target  = self::EMERGENCY_NUMBER .'@'. $sip_uri;
         $channel_a = self::ensureChannel( self::$a_device_1->originate($target) );
         $channel_b = self::ensureChannel( self::$emergency_resource->waitForInbound(self::EMERGENCY_NUMBER) );
-        self::assertEquals(
-            self::$a_user->getCidParam("emergency")->number,
-            urldecode($channel_b->getEvent()->getHeader("Caller-Caller-ID-Number"))
-        );
+        $a_cidnum = self::$a_user->getCidParam("emergency")->number;
+        $a_cidname = self::$a_user->getCidParam("emergency")->name;
+        $b_cidnum = urldecode($channel_b->getEvent()->getHeader("Caller-Caller-ID-Number"));
+        $b_cidname = urldecode($channel_b->getEvent()->getHeader("Caller-Caller-ID-Name"));
+        self::assertEquals($a_cidnum, $b_cidnum, "Emergency Number - " . $a_cidnum . " - " . $b_cidnum);
+        self::assertEquals($a_cidname, $b_cidname, "Emergency Name - " . $a_cidname . " - " . $b_cidname);
         self::ensureAnswer($channel_a, $channel_b);
         self::hangupBridged($channel_a, $channel_b);
     }
